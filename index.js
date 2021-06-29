94736195bd8df351179790757767de395031e857
Michael Robards <mikerobards@icloud.com>
2021-06-29 15:40:13 -0400

    add photo credit

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
+const author = document.querySelector('.img-author')
+
+
 async function getBackgroundImg() {
-    const response = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    const data = await response.json()
-    console.log(data.urls.full)
+    const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    const data = await res.json()
+    console.log(data)
     document.body.style.backgroundImage = `url("${data.urls.full}")`
+    author.innerText = `Photo By ${data.user.name}`
 }
 
 getBackgroundImg()
 
-// async function newDeck() {
-//     const response = await fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-//     const data = await response.json()
-//     remainingText.textContent = `Remaining cards: ${data.remaining}`
-//     deckId = data.deck_id
-//     console.log(deckId)
-// }
