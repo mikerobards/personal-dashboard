cd45e59d87045ccf2cfa1ed6f7347717542986d3
Michael Robards <mikerobards@icloud.com>
2021-07-01 16:51:03 -0400

    add error handling to getBackgroundImg function

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const author = document.querySelector('.img-author')
 
 
-async function getBackgroundImg() {
-    const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    const data = await res.json()
-    console.log(data)
-    document.body.style.backgroundImage = `url("${data.urls.full}")`
-    author.innerHTML = `<p>Photo By <a href="${data.user.links.html}" target="_blank">${data.user.name}</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Unsplash</a></p>`
+function getBackgroundImg() {
+    fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=naasdfasdfasdfasdafsdfture")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            document.body.style.backgroundImage = `url("${data.urls.full}")`
+            author.innerHTML = `<p>Photo By <a href="${data.user.links.html}" target="_blank">${data.user.name}</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Unsplash</a></p>`
+        })
+        .catch(err => {
+            document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1571944400884-bc70b2e2ed87?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjUxNzE2NTk&ixlib=rb-1.2.1&q=85")`
+            author.innerHTML = `<p>Photo By <a href="https://unsplash.com/@benkelsey?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Ben Kelsey</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Unsplash</a></p>`
+        })
 }
 
 getBackgroundImg()
-
-
-
 
 
